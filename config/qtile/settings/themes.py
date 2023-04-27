da403c5ceed0980beae7c82abe2ea030bc4a75e0
Cless <marcelperezcodelia@gmail.com>
2023-04-27 15:18:52 -0400

    Added catppuccin (latte, frappe, mocha) and gruvbox themes

--- config/qtile/settings/themes.py
+++ config/qtile/settings/themes.py
@@ -9,4 +9,48 @@ nord =  {
     "green":        "#a3be8c"
         }
 
-theme = nord
+# https://github.com/morhetz/gruvbox
+gruvbox = {
+    "background":   "#282828",  # bg
+    "background2":  "#32302f",  # bg0_s
+    "foreground":   "#ebdbb2",  # fg
+    "primary":      "#b16286",  # purple
+    "secondary":     "#689d6a", # aqua
+    "red":          "#cc241d",  # red
+    "green":        "#98971a"   # green
+        }
+
+# https://github.com/catppuccin/catppuccin 
+catppuccin_latte = {
+    "background":   "#eff1f5",  # Base
+    "background2":  "#ccd0da",  # Surface0
+    "foreground":   "#4c4f69",  # Text
+    "primary":      "#ea76cb",  # Pink 
+    "secondary":     "#dd7878", # Flamingo 
+    "red":          "#d20f39",  # Red
+    "green":        "#40a02b"   # Green
+        }
+
+catppuccin_frappe = {
+    "background":   "#303446",  # Base
+    "background2":  "#414559",  # Surface0
+    "foreground":   "#c6d0f5",  # Text
+    "primary":      "#ca9ee6",  # Mauve
+    "secondary":     "#f4b8e4", # Pink
+    "red":          "#e78284",  # Red
+    "green":        "#a6d189"   # Green
+        }
+
+catppuccin_mocha = {
+    "background":   "#1e1e2e",  # Base
+    "background2":  "#313244",  # Surface0
+    "foreground":   "#cdd6f4",  # Text
+    "primary":      "#cba6f7",  # Mauve
+    "secondary":     "#b4befe", # Lavender
+    "red":          "#f38ba8",  # Red
+    "green":        "#a6e3a1"   # Green
+        }
+
+
+
+theme = nord 
