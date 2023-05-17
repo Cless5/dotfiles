ce8e553d613f6db7673a1e51de45dcaf7f24304c
Cless <marcelperezcodelia@gmail.com>
2023-05-17 14:54:41 -0400

    Modified tab behavior

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -1,13 +1,13 @@
 local o = vim.o
 local g = vim.g
 
-o.number		= true
-o.tabstop		= 4
-o.shiftwidth	= 4
-o.softtabstop	= 4
+o.number			= true
+o.tabstop			= 2
+o.shiftwidth	= 2
+o.softtabstop	= 2
 o.cursorline	= true
 o.termguicolors	= true
-o.backup		= false
+o.backup			= false
 o.writebackup	= false
 o.updatetime	= 300
 o.signcolumn	= "yes"
